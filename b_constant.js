cffecfa8aa84e45c64a1629a1f761555ceccaa29
Yogiram727 <yogiram727@gmail.com>
2024-01-11 20:05:50 +0545

    constant

---
+++ b_constant.js
@@ -0,0 +1,6 @@
+const interestRate=0.3;
+console.log(interestRate);
+
+//value of variable change but constant doesn't change 
+//if you don't want to reassign then constant 
+//if you want to reassign then use let
