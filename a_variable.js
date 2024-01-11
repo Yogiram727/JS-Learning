3b1f2be211ae77784e82df181c4105ef15f43630
Yogiram727 <yogiram727@gmail.com>
2024-01-11 19:43:26 +0545

    variable

---
+++ a_variable.js
@@ -0,0 +1,11 @@
+let fname='Ram';
+console.log(fname);
+
+//cannot be a reserved keyword
+//should be meaningful
+//cannot start with a number e.g. 2name
+//cannot contain space or hyphen (-)
+//are case-sensitive 
+
+let firstName='Ram';
+let lastName='Nath';
