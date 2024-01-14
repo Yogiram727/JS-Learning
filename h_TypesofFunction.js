e864232bec8ec972876847f973cf1f0c8152dbb6
Ram Nath <yogiram727@gmail.com>
2024-01-14 08:35:41 +0545

    typesoffunction

---
+++ h_TypesofFunction.js
@@ -0,0 +1,11 @@
+//block of statements that perform task
+function greet(name,lastName){
+    console.log('Hello '+name+' '+lastName);
+}
+
+//calculating a value
+function square(number){
+    return number*number;
+}
+console.log(square(2));
+greet('Ram','Nath');
