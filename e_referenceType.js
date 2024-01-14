8489914d59e43459d7c6425af59b2b56b8404c31
Ram Nath <yogiram727@gmail.com>
2024-01-14 08:18:51 +0545

    objects

---
+++ e_referenceType.js
@@ -0,0 +1,17 @@
+//Object is a real world entity
+//we can put multiple related variable inside object
+
+// let name='Ram';
+// let age='25';
+let person={
+    name:'Ram',
+    age:'25'
+};
+//Dot Notation
+person.name='Raman';
+
+//Bracket Notation
+person['name']='Hari'; //when we don't know what property we are going to access
+
+console.log(person);
+console.log(person.name);
