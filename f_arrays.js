93d6aa8504d27fa7bff91b97eabe316190fa6e24
Ram Nath <yogiram727@gmail.com>
2024-01-14 08:24:46 +0545

    arrays

---
+++ f_arrays.js
@@ -0,0 +1,11 @@
+//when dealing with list of objects
+
+let selectedColors = ['red','blue'];//type of array is dyamic
+selectedColors[2]='green'; //length is dynamic
+selectedColors[3]=1; 
+
+console.log(selectedColors);
+console.log(selectedColors[0]);
+
+//Array is object
+console.log(selectedColors.length);
