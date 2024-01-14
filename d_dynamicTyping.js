f14d3bbbe05678e6fba0444ce7f1a62bc24b2aa4
Ram Nath <yogiram727@gmail.com>
2024-01-14 08:12:55 +0545

    dynamic typing

---
+++ d_dynamicTyping.js
@@ -0,0 +1,12 @@
+//type of variable is determined during the runtime or can be changed during runtime
+let name='Mosh'; //String Literal
+var age=30; //Number Literal
+console.log(typeof age);
+let isApproved=true; //Boolean Literal
+let firstName=undefined;
+let selectColor=null;// type of this variable is object
+var age='hello';
+console.log(typeof age);
+console.log(typeof isApproved);
+console.log(typeof firstName);// type is also undefined
+console.log(typeof selectColor);
