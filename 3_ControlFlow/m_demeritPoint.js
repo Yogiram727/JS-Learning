1cab0bd65275f2bbc96040fcb161faa66ec5d633
Ram Nath <yogiram727@gmail.com>
2024-01-17 12:09:05 +0545

    demeritpoints

---
+++ 3_ControlFlow/m_demeritPoint.js
@@ -0,0 +1,24 @@
+ //Speed Limit =70
+ //5 -> 1 point
+ //Math.floor(1.3)
+ //12 points => Suspended
+
+ checkSpeed(84);
+
+ function checkSpeed(speed){
+    const speedLimit=70;
+    const kmPerPoint=5;
+    
+    if(speed < speedLimit +kmPerPoint){
+        console.log('Ok');
+        return;
+    }
+    const points=Math.floor((speed-speedLimit)/kmPerPoint);
+    if(points >=12)
+        console.log('License Suspended');
+    else
+        console.log('Points',points);
+    
+ }
+
+
