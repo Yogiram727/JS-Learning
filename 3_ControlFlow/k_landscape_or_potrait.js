672ba4a5b66494fa05db0805b34417c17a7e3843
Ram Nath <yogiram727@gmail.com>
2024-01-17 10:28:33 +0545

    exercises

---
+++ 3_ControlFlow/k_landscape_or_potrait.js
@@ -0,0 +1,9 @@
+console.log(isLandScape(800,600));
+
+function isLandScape(width,height){
+    // if(width>height)return true;
+    // return false;
+    
+    // return (width>height)?true:false;
+    return (width>height);
+}
